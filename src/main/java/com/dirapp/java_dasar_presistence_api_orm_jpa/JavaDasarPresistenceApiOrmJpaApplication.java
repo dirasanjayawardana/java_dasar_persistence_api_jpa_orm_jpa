e3fd9c18d75316af3941c6ee067e1f9693385c95
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-09-01 13:46:28 +0700

    backup

---
+++ src/main/java/com/dirapp/java_dasar_presistence_api_orm_jpa/JavaDasarPresistenceApiOrmJpaApplication.java
@@ -0,0 +1,13 @@
+package com.dirapp.java_dasar_presistence_api_orm_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDasarPresistenceApiOrmJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDasarPresistenceApiOrmJpaApplication.class, args);
+	}
+
+}
